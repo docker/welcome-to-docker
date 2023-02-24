1d5c6b56ea246b112792e4eb9e6c79ec2d782295
Nuno Coração <nunocoracao@gmail.com>
2023-02-24 21:56:14 +0000

    polish

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import logo from './baby_moby.png';
 import './App.css';
 import Confetti from './Confetti';
 
-const shareMessage = 'I just published an article on how you can share content to Twitter from the client website'
+const shareMessage = 'I just ran my first container using Docker'
 const shareLink = 'https://docker.com/'
 
 const App = () => {
   return (
     <div className="App">
-      <Confetti/>
+      <Confetti />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 style={{ marginBottom: "0px" }}>
           Congratulations!!!
         </h1>
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
-          You just run your first container.
+          You ran your first container.
         </p>
         <div>
-          <a target="_blank" href={"https://twitter.com/intent/tweet?text=" + shareMessage + "&url=" + shareLink} class="fa fa-twitter" rel="noopener noreferrer"></a>
-          <a target="_blank" href={"https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink} class="fa fa-linkedin" rel="noopener noreferrer"></a>
-          <a target="_blank" href={"https://reddit.com/submit?title=" + shareMessage + "&url=" + shareLink} class="fa fa-reddit" rel="noopener noreferrer"></a>
+          <a target="_blank" href={"https://twitter.com/intent/tweet?text=" + shareMessage + "&url=" + shareLink} class="fa fa-twitter" rel="noopener noreferrer"> </a>
+          <a target="_blank" href={"https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink} class="fa fa-linkedin" rel="noopener noreferrer"> </a>
+          <a target="_blank" href={"https://reddit.com/submit?title=" + shareMessage + "&url=" + shareLink} class="fa fa-reddit" rel="noopener noreferrer"> </a>
         </div>
       </header>
     </div>
