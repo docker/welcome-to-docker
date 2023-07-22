e0e20da923ee54cac59f3a18da3da2195d1f272d
Ito <carloscampostorres1990@gmail.com>
2023-07-22 09:17:15 -0400

    create methods

--- javascript syntax part II/Team Stats.js
+++ javascript syntax part II/Team Stats.js
@@ -2,12 +2,16 @@
 Our team has players and the games that they have played. Let’s represent both of these by adding two properties to your team object. Add a _players property and a _games property and initialize both to empty arrays.
 Next, populate the empty_players array with three players. Each player should be an object containing three properties: firstName, lastName, and age. Put each player on a new line to prevent the line from getting too long.
 Let’s do the same for our _games array. Populate the empty array with three games. Each game should be an object containing three properties: opponent, teamPoints, opponentPoints.
+Create a getter method called players to retrieve the _players property. Inside the getter method, return the _players property.
+For the scope of this project, we won’t need to create setter methods, because we don’t want anyone to change the data saved to the properties.
 */
 
 const team = {};
 
-team._players = ['firstName', 'lastName', 'age'];
+team._players = ['firstName', 'lastName', 'age']; 
+team._players.push();
 
 team._games = ['opponent', 'teamPoints', 'opponentPoints'];
+team._games.push();
 
 
