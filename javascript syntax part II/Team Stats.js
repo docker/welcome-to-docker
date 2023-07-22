d5f884ecfb64c27f8338f45a9cf1116364e0fa31
Ito <carloscampostorres1990@gmail.com>
2023-07-22 09:04:40 -0400

    Team Stats project

---
+++ javascript syntax part II/Team Stats.js
@@ -0,0 +1,11 @@
+/*Let’s make a data structure to store the information about our team. Declare a const variable called team and set it to an empty object.
+Our team has players and the games that they have played. Let’s represent both of these by adding two properties to your team object. Add a _players property and a _games property and initialize both to empty arrays.
+*/
+
+const team = {};
+
+team._players = [];
+
+team._games = [];
+
+
