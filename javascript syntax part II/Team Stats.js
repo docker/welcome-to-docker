fa7f949a75a4cd6e64dff3c2d8424b5314184241
Ito <carloscampostorres1990@gmail.com>
2023-07-22 09:11:07 -0400

    populate the empty arrays

--- javascript syntax part II/Team Stats.js
+++ javascript syntax part II/Team Stats.js
@@ -1,11 +1,13 @@
 /*Let’s make a data structure to store the information about our team. Declare a const variable called team and set it to an empty object.
 Our team has players and the games that they have played. Let’s represent both of these by adding two properties to your team object. Add a _players property and a _games property and initialize both to empty arrays.
+Next, populate the empty_players array with three players. Each player should be an object containing three properties: firstName, lastName, and age. Put each player on a new line to prevent the line from getting too long.
+Let’s do the same for our _games array. Populate the empty array with three games. Each game should be an object containing three properties: opponent, teamPoints, opponentPoints.
 */
 
 const team = {};
 
-team._players = [];
+team._players = ['firstName', 'lastName', 'age'];
 
-team._games = [];
+team._games = ['opponent', 'teamPoints', 'opponentPoints'];
 
 
