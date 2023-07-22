fdcd7256eaeb1f39fc15a2e1f989b85b87a5e930
Ito <carloscampostorres1990@gmail.com>
2023-07-22 09:19:22 -0400

    changes

--- javascript syntax part II/Team Stats.js
+++ javascript syntax part II/Team Stats.js
@@ -4,14 +4,21 @@ Next, populate the empty_players array with three players. Each player should be
 Let’s do the same for our _games array. Populate the empty array with three games. Each game should be an object containing three properties: opponent, teamPoints, opponentPoints.
 Create a getter method called players to retrieve the _players property. Inside the getter method, return the _players property.
 For the scope of this project, we won’t need to create setter methods, because we don’t want anyone to change the data saved to the properties.
+Create another getter method called games to retrieve the _games property. Inside the getter method, return the _games property.
+Below the team object, let’s try out our new .addPlayer() method to add a new player: Bugs Bunny, age 76.
+Log the team‘s _players property to check that our new player was added.
 */
 
 const team = {};
 
 team._players = ['firstName', 'lastName', 'age']; 
-team._players.push();
+team._players.push(
+    {firstName: 'XXXX', lastName: 'Bunny', age: 76}  
+);
 
 team._games = ['opponent', 'teamPoints', 'opponentPoints'];
-team._games.push();
+team._games.push(
+    {opponent: 'YYYYY', teamPoints: 76, opponentPoints: 76}
+);
 
 
