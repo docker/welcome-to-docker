8c2a588ac8a635868b9becc72dfb0833d82f523b
marisimon <104405305+marisimon@users.noreply.github.com>
2023-10-24 10:47:12 -0600

    Update twitter icon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import './App.css';
-import Confetti from './Confetti';
+import "./App.css";
+import Confetti from "./Confetti";
 
-const shareMessage = 'I just ran my first container using Docker'
-const shareLink = 'https://docker.com/'
+const shareMessage = "I just ran my first container using Docker";
+const shareLink = "https://docker.com/";
 
 const App = () => {
   return (
     <div className="App">
       <Confetti />
       <header className="App-header">
-        <h1 style={{ marginBottom: "0px" }}>
-          Congratulations!!!
-        </h1>
+        <h1 style={{ marginBottom: "0px" }}>Congratulations!!!</h1>
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
           You ran your first container.
         </p>
         <div>
-          <a target="_blank" href={"https://twitter.com/intent/tweet?text=" + shareMessage + "&url=" + shareLink} class="fa fa-twitter" rel="noopener noreferrer"> </a>
-          <a target="_blank" href={"https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink} class="fa fa-linkedin" rel="noopener noreferrer"> </a>
-          <a target="_blank" href={"https://reddit.com/submit?title=" + shareMessage + "&url=" + shareLink} class="fa fa-reddit" rel="noopener noreferrer"> </a>
+          <a
+            target="_blank"
+            href={
+              "https://twitter.com/intent/tweet?text=" +
+              shareMessage +
+              "&url=" +
+              shareLink
+            }
+            class="fa-brands fa-x-twitter"
+            rel="noopener noreferrer"
+          >
+            {" "}
+          </a>
+          <a
+            target="_blank"
+            href={
+              "https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink
+            }
+            class="fa-brands fa-linkedin"
+            rel="noopener noreferrer"
+          >
+            {" "}
+          </a>
+          <a
+            target="_blank"
+            href={
+              "https://reddit.com/submit?title=" +
+              shareMessage +
+              "&url=" +
+              shareLink
+            }
+            class="fa-brands fa-reddit"
+            rel="noopener noreferrer"
+          >
+            {" "}
+          </a>
         </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
